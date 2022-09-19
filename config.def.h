85c915bd42c27a0e694dfa243ec37ac964e5e26c
Elizarov Dmitry <d.elizarov.00@gmail.com>
2022-09-19 11:03:41 +0300

    brown filthy theme

--- config.def.h
+++ config.def.h
@@ -15,11 +15,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka:size=14" };
 static const char dmenufont[]       = "Iosevka:size=14";
-static const char col_gray1[]       = "#002b36";
-static const char col_gray2[]       = "#073642";
+static const char col_gray1[]       = "#1d2021";
+static const char col_gray2[]       = "#32302f";
 static const char col_gray3[]       = "#839496";
-static const char col_gray4[]       = "#073642";
-static const char col_cyan[]        = "#689d6a";
+static const char col_gray4[]       = "#d4be98";
+static const char col_cyan[]        = "#45403d";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -75,7 +75,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "1"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
